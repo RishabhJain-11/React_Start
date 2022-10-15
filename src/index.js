a83b75ad3608a6b298bfc2e2c3c0237b1acd2899
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-15 22:59:58 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import './index.css'
+
 
 function BookList() {
-  return <h1>This is Rishabh Jain</h1>
-} 
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => {
+  return <img src="https://m.media-amazon.com/images/I/715vXuwzxyL._AC_UY327_FMwebp_QL65_.jpg" alt="" />
+}
+
+const Title = () => {
+  return <h1>Atal Bihari Vajpayee: A Man for All Seasons</h1>
+}
+
+const Author = () => {
+  return <h4>Rishabh Jain</h4>
+}
 ReactDOM.render(<BookList />, document.getElementById('root'));
