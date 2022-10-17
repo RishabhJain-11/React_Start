4c9cb0df33597adf0b7e948a0ebff86a8956cbed
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-17 18:53:17 +0530

    ..

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/715vXuwzxyL._AC_UY327_FMwebp_QL65_.jpg",
+        author: 'Rishabh Jain',
+        title: 'Atal Bihari Vajpayee: A Man for All Seasons'
+    },
+    {
+        id: 2,
+        img: "https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY327_FMwebp_QL65_.jpg",
+        author: 'Morgan Housel',
+        title: 'The Psychology of Money'
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/61kRkfsIMUL._AC_UY327_FMwebp_QL65_.jpg",
+        author: 'Ankur Warikoo',
+        title: 'DO EPIC SHIT'
+    }
+]
--- src/index.js
+++ src/index.js
@@ -1,65 +1,19 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css'
-
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/715vXuwzxyL._AC_UY327_FMwebp_QL65_.jpg",
-  author: 'Rishabh Jain',
-  title: 'Atal Bihari Vajpayee: A Man for All Seasons'
-}
-
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY327_FMwebp_QL65_.jpg",
-  author: 'Morgan Housel',
-  title: 'The Psychology of Money'
-}
-
-const thirdBook = {
-  img: "https://m.media-amazon.com/images/I/61kRkfsIMUL._AC_UY327_FMwebp_QL65_.jpg",
-  author: 'Ankur Warikoo',
-  title: 'DO EPIC SHIT'
-}
+import './index.css';
+import { books } from './books';
+import Book from './Book'
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-      </Book>
-
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>
+      })}
     </section>
-  )
-}
+  );
+};
 
-const Book = (props) => {
-  const { img, title, author } = props;
-  const clickHandler = () => {
-    alert("Choose any of the books fast");
-  }
-  return (
-    <article className="book">
-      <img src={img} alt="" />
 
-      <h1 onClick={clickHandler}>{title}</h1>
-
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>Reference Example</button>
-    </article>
-  )
-}
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
